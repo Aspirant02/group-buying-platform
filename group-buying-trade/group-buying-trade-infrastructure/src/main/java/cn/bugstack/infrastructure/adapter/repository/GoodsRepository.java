f3775183518a11c1c0312adc95851aaa8f41e779
fzh <fzh13593596284@163.com>
2026-06-30 02:11:56 +0800

    feat: add cleaned group buying source modules

---
+++ group-buying-trade/group-buying-trade-infrastructure/src/main/java/cn/bugstack/infrastructure/adapter/repository/GoodsRepository.java
@@ -0,0 +1,24 @@
+package cn.bugstack.infrastructure.adapter.repository;
+
+import cn.bugstack.domain.goods.adapter.repository.IGoodsRepository;
+import cn.bugstack.infrastructure.dao.IOrderDao;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
+
+/**
+ * @description 结算仓储服务
+ * @create 2025-02-15 09:13
+ */
+@Repository
+public class GoodsRepository implements IGoodsRepository {
+
+    @Resource
+    private IOrderDao orderDao;
+
+    @Override
+    public void changeOrderDealDone(String orderId) {
+        orderDao.changeOrderDealDone(orderId);
+    }
+
+}
